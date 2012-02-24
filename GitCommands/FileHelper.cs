6d4d24379a71f0dbfc1a4aa0c8df35801a07e91a
Dominique Plante <dominique.plante@gmail.com>
2012-02-24 08:37:38 -0800

    flip around some conditionals, return early in for loop when too much binary content is detected

--- GitCommands/FileHelper.cs
+++ GitCommands/FileHelper.cs
@@ -52,7 +52,7 @@ namespace GitCommands
             ".jpeg",
             ".png",
             ".tif",
-            ".tiff",
+            ".tiff"
         };
 
         public static bool IsBinaryFile(string fileName)
@@ -66,26 +66,26 @@ namespace GitCommands
         /// <returns>null if no info in .gitattributes. True if marked as binary, false if marked as text</returns>
         private static bool? IsBinaryAccordingToGitAttributes(string fileName)
         {
-            string gitAttributesPath = Path.Combine(GitCommands.Settings.WorkingDir, ".gitattributes");
-            if (File.Exists(gitAttributesPath))
+            string gitAttributesPath = Path.Combine(Settings.WorkingDir, ".gitattributes");
+            if (!File.Exists(gitAttributesPath))
             {
-                string[] lines = File.ReadAllLines(gitAttributesPath);
-                bool? lastMatchResult = null;
-                foreach (var parts in lines.Select(line => line.Trim().Split(' ')))
-                {
-                    if (parts.Length < 2 || parts[0][0] == '#')
-                        continue;
-                    if (parts.Contains("binary") || parts.Contains("-text"))
-                        if (Regex.IsMatch(fileName, CreateRegexFromFilePattern(parts[0])))
-                            lastMatchResult = true;
-                    if (parts.Contains("text"))
-                        if (Regex.IsMatch(fileName, CreateRegexFromFilePattern(parts[0])))
-                            lastMatchResult = false;
-                }
-                return lastMatchResult;
+                return null;
             }
 
-            return null;
+            string[] lines = File.ReadAllLines(gitAttributesPath);
+            bool? lastMatchResult = null;
+            foreach (var parts in lines.Select(line => line.Trim().Split(' ')))
+            {
+                if (parts.Length < 2 || parts[0][0] == '#')
+                    continue;
+                if (parts.Contains("binary") || parts.Contains("-text"))
+                    if (Regex.IsMatch(fileName, CreateRegexFromFilePattern(parts[0])))
+                        lastMatchResult = true;
+                if (parts.Contains("text"))
+                    if (Regex.IsMatch(fileName, CreateRegexFromFilePattern(parts[0])))
+                        lastMatchResult = false;
+            }
+            return lastMatchResult;
         }
 
         private static string CreateRegexFromFilePattern(string pattern)
@@ -107,40 +107,32 @@ namespace GitCommands
         public static bool IsBinaryFileAccordingToContent(byte[] content)
         {
             //Check for binary file.
-            if (content != null && content.Length > 0)
+            if (content == null || content.Length <= 0)
+                return false;
+            int nullCount = 0;
+            foreach (char c in content)
             {
-                int nullCount = 0;
-                foreach (char c in content)
-                {
-                    if (c == '\0')
-                        nullCount++;
-                    if (nullCount > 5) break;
-                }
-
+                if (c == '\0')
+                    nullCount++;
                 if (nullCount > 5)
                     return true;
             }
-
             return false;
         }
 
         public static bool IsBinaryFileAccordingToContent(string content)
         {
             //Check for binary file.
-            if (!string.IsNullOrEmpty(content))
+            if (string.IsNullOrEmpty(content))
+                return false;
+            int nullCount = 0;
+            foreach (char c in content)
             {
-                int nullCount = 0;
-                foreach (char c in content)
-                {
-                    if (c == '\0')
-                        nullCount++;
-                    if (nullCount > 5) break;
-                }
-
+                if (c == '\0')
+                    nullCount++;
                 if (nullCount > 5)
                     return true;
             }
-
             return false;
         }
         #endregion
