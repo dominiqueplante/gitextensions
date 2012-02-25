3907502cd353cf6c173cae2b466fa56e6b8cd78d
Dominique Plante <dominique.plante@gmail.com>
2012-02-25 12:18:45 -0800

    remove unused directives

--- GitCommandsTests/CommitInformationTest.cs
+++ GitCommandsTests/CommitInformationTest.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
 using GitCommands;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
--- GitCommandsTests/ConfigFileTest.cs
+++ GitCommandsTests/ConfigFileTest.cs
@@ -4,7 +4,6 @@ using System.Text;
 using GitCommands.Config;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
-using GitCommands;
 
 namespace GitCommandsTests
 {
